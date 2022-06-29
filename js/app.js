a48490201bbdeead9504bd78bf9ff705ec58ebad
Francisco <ramirezcrisantofranciscojavier@gmail.com>
2022-06-29 15:37:51 -0500

    se agrego dos funciones (llamadas amarillo y white) su proposito es cambiar de color al texto, del titulo Match Game

---
+++ js/app.js
@@ -0,0 +1,11 @@
+//metodos setInterval
+setInterval('amarillo()',3000);
+setInterval('white()',5000);
+
+function amarillo(){
+  $(".main-titulo").css("color","yellow")
+}
+
+function white(){
+  $(".main-titulo").css("color","white")
+}
